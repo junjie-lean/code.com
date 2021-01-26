8a7198e8a87da87347b039f1ef92f679a69a6afd
junjie-lean <junjie.lean@foxmail.com>
2021-01-27 00:06:04 +0800

    Environment construction and processing
    tsc -w
    nodemon src/entry.js -w src -i src/*.ts

--- src/entry.ts
+++ src/entry.ts
@@ -2,19 +2,117 @@
  * @Author: junjie.lean
  * @Date: 2021-01-26 15:09:42
  * @Last Modified by: junjie.lean
- * @Last Modified time: 2021-01-26 17:31:31
+ * @Last Modified time: 2021-01-26 23:55:05
  */
+
+/**
+ * @version 0.0.1
+ * @description :  code compute and statistics result by file author
+ * @muduleExport : statistics result
+ */
+
 import { promises as fsPromise } from "fs";
+import * as path from "path";
+
 import toolConfig from "./toolConfig";
 
-export default class CodeCom {
-  static lookConfig() {
-    console.log(toolConfig);
-    console.log(fsPromise);
-  }
-  dev() {
-    console.log(toolConfig);
+const { authorKey, modifyKey, includeDir, ignoreDir } = toolConfig;
+
+//root dir
+//code author list
+const authorList: Array<any> = [];
+
+//code file header list
+const fileStatisticsList: Array<any> = [];
+/**
+ * @description  recursion dispose dir,
+ * if target is Directory, recursion dispose it,
+ * or if target is file, call readSize( file path ) function
+ * @param { String }  _path
+ * @returns null
+ */
+async function recursionDisposeDir(_path: string) {
+  let currentStats: any = await fsPromise.stat(_path);
+
+  const currentPathIsDir: boolean = currentStats.isDirectory();
+
+  if (currentPathIsDir) {
+    //if current path is dir , check it's in ignore list?
+
+    const currentDirIsInIgnoreList: boolean = ignoreDir.some((item) => {
+      const currentDirName = _path.slice(_path.lastIndexOf("\\") + 1);
+      return item === currentDirName;
+    });
+
+    // console.log( _path.slice(_path.lastIndexOf("\\") + 1), '\n',_path);
+    if (currentDirIsInIgnoreList) {
+      //current dir is in ignore list , continue
+      // console.log("do not dispose this path:", _path);
+    } else {
+      let lsDir = await fsPromise.readdir(_path);
+
+      lsDir.map((item) => {
+        let childrenPath = path.resolve(_path, item);
+        recursionDisposeDir(childrenPath);
+      });
+    }
+  } else {
+    await readFileInfo(_path);
   }
 }
 
-CodeCom.lookConfig();
+/**
+ * @description set authorList value
+ * @param { String } _path
+ * @returns file info object
+ */
+const readFileInfo = async function (_path: string) {
+  let fileContent: string = await (await fsPromise.readFile(_path)).toString();
+  let fileStat: any = await fsPromise.stat(_path);
+
+  let fileHeader: Array<string> = fileContent
+    .slice(2, fileContent.indexOf("*/"))
+    .split(" * ")
+    .filter((item) => {
+      return item != "\r\n";
+    })
+    .map((item) => {
+      return item ? item.replace("\r\n", "").replace("@", "").trim() : "";
+    });
+
+  let fileAuthor: string = "";
+  let fileLastModify: string = "";
+
+  fileHeader.map(function (item: string): void {
+    if (item.indexOf(authorKey) > -1) {
+      //if find author key,dispose fileAuthor
+      fileAuthor = item.slice(authorKey.length + 1).trim();
+    }
+
+    if (item.indexOf(modifyKey) > -1) {
+      //but if find this string key is modifyKey,dispose lastmodify key
+      fileLastModify = item.slice(modifyKey.length + 1).trim();
+    }
+  });
+
+  // console.log("fileAuthor:", fileHeader);
+  // console.log("fileLastModify:", fileLastModify);
+
+  let fileInfo = {
+    fileName: path.basename(_path),
+    fileSize: fileStat.size,
+    fileSizeUtil: "byte",
+    // fileHeader,
+    fileAuthor,
+    fileLastModify,
+  };
+  // console.log(fileInfo);
+  authorList.push(fileInfo);
+};
+
+(async () => {
+  includeDir.map(function (item: string): any {
+    recursionDisposeDir(item);
+  });
+  // console.log("result : ", authorList);
+})();
--- src/toolConfig.ts
+++ src/toolConfig.ts
@@ -2,12 +2,15 @@
  * @Author: junjie.lean
  * @Date: 2021-01-26 15:11:15
  * @Last Modified by: junjie.lean
- * @Last Modified time: 2021-01-26 17:18:00
+ * @Last Modified time: 2021-01-26 23:36:52
  */
 
 const toolConfig = {
-  incloudDri: ["src"],
-  ignoreDir: ["media", "util"],
+  includeDir : ["target"],
+  ignoreDir: ["media", "util","lang","test","style"],
+  // statisticsRootDirectoryName: "src",
+  authorKey: "Author",
+  modifyKey: "Last Modified by",
 };
 
 export default toolConfig;
