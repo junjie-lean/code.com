831d6dbbcfc97284022898736ec3174d1516ee62
junjie-lean <40194410+junjie-lean@users.noreply.github.com>
2021-01-26 14:53:19 +0800

    initial commit

---
+++ config/webpack.config.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: junjie.lean 
+ * @Date: 2021-01-26 14:45:38 
+ * @Last Modified by: junjie.lean
+ * @Last Modified time: 2021-01-26 14:47:02
+ */
+
+
+ /**
+  * @description 
+  * @export webpack config 
+  */
---
+++ index.js
@@ -0,0 +1,6 @@
+/*
+ * @Author: junjie.lean
+ * @Date: 2021-01-26 14:31:04
+ * @Last Modified by:   junjie.lean
+ * @Last Modified time: 2021-01-26 14:31:04
+ */
